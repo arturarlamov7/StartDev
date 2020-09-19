05e41e01637a94f5ec67a328888ff6ca893eb07b
arturarlamov7 <58815804+arturarlamov7@users.noreply.github.com>
2020-09-19 14:26:34 +0300

    task2

--- src/startdev/Task2.java
+++ src/startdev/Task2.java
@@ -1,31 +1,44 @@
-package startdev;
-
-import java.util.Random;
-import java.util.Scanner;
-
-
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package startdev;
+
+import java.util.Scanner;
 
 /**
  *
- * @author user
+ * @author artur
  */
-public class Task2 {
-    public void run(){
-        System.out.println("----- Задача 2 ------");
-        Random rand = new Random();
-        int Number1 = rand.nextInt(5);
-        int Tries = 0;
+class Task2 {
+    public void run() { 
+        int UnknownNumber, UserNumber, TrysCount = 0;
         Scanner input = new Scanner(System.in);
-        int guess;
-        boolean win = false;
 
-        
-        System.out.println("----- конец задачи 2 ------");
-    } 
+        System.out.println("Загадано число от 1 до 5");
+
+        UnknownNumber = (int)Math.floor(Math.random() * 5);
+
+        do {
+		TrysCount++;
+
+		System.out.print("Введите ваше число: ");
+
+		UserNumber = input.nextInt();
+				
+		if (UserNumber > UnknownNumber)
+		System.out.println("Ты проиграл!");
+            
+		else if (UserNumber < UnknownNumber)            	
+		System.out.println("Ты проиграл!");	 		 		
+
+		else 
+		System.out.println("Ты выйграл!");
+
+        } while (UserNumber != UnknownNumber);
+
+        System.out.println("Количество попыток: " + TrysCount);
+
+    }
 }
